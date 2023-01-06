9f3096a17abae49f202a3a105ffc76df7631432b
Kim Stosik <kstosik@uni-bremen.de>
2023-01-06 16:26:15 +0100

    added design and home screen layout, added more buttons and icons

---
+++ DiaBeatThis/lib/utils/constants.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+//COLOR
+const COLOR_INDIGO = Colors.indigo;
+const COLOR_INDIGO_LIGHT = Color(0xff7c7db6);
+const COLOR_WHITE = Colors.white;
+const COLOR_RED = Colors.red;
+const COLOR_BLACK = Colors.black87;
+
+//TEXTSTYLE
+const TextStyle DIABEATTHIS_LOGO =
+    TextStyle(color: COLOR_WHITE, fontSize: 25, fontFamily: 'Aloja');
+const TextStyle HEADLINE_BOLD_BLACK =
+    TextStyle(color: COLOR_BLACK, fontSize: 21, fontFamily: 'VisbyDemiBold');
+const TextStyle HEADLINE_BOLD_WHITE =
+    TextStyle(color: COLOR_WHITE, fontSize: 21, fontFamily: 'VisbyDemiBold');
+const TextStyle TEXT_PLAIN =
+    TextStyle(color: COLOR_BLACK, fontSize: 14, fontFamily: 'VisbyMedium');
+const TextStyle TEXT_BOLD =
+    TextStyle(color: COLOR_BLACK, fontSize: 14, fontFamily: 'VisbyDemiBold');
+const TextStyle HOME_POST_CREATOR =
+    TextStyle(color: COLOR_BLACK, fontSize: 16, fontFamily: 'VisbyDemiBold');
+const TextStyle LOGBUTTON =
+    TextStyle(color: COLOR_INDIGO, fontSize: 16, fontFamily: 'VisbyDemiBold');
+
+//ICON SIZE
+const double PROFILE_ICON_BAR_SIZE = 43;
+const double USER_ICON_POST_SIZE = 38;

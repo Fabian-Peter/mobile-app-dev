a609662100c2d18973587a7fc9cb7f4cd9b40d75
Kim Stosik <kstosik@uni-bremen.de>
2023-01-07 12:42:45 +0100

    improved design in post screen

--- DiaBeatThis/lib/screens/post_screen.dart
+++ DiaBeatThis/lib/screens/post_screen.dart
@@ -6,6 +6,7 @@ import '../classes/Post.dart';
 
 class PostScreen extends StatelessWidget {
   final Post post;
+  IconData _favIconOutlined = Icons.favorite_outline;
 
   PostScreen({required this.post});
 
@@ -48,57 +49,6 @@ class PostScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildDescription(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.only(top: 20),
-        child: Text(post.recipe.description, style: TEXT_PLAIN));
-  }
-
-  Widget _buildNutrition() {
-    const double iconSize = 54;
-    return Row(children: [
-      const SizedBox(width: 43),
-      Column(children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(iconSize / 2),
-          child: Image.asset(
-            'assets/images/Fats.png',
-            height: iconSize,
-            width: iconSize,
-          ),
-        ),
-        const SizedBox(height: 5),
-        const Text("Fats: 5g", style: TEXT_PLAIN) //TODO: replace with post values
-      ]),
-      const SizedBox(width: 30),
-      Column(children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(iconSize / 2),
-          child: Image.asset(
-            'assets/images/Carbs.png',
-            height: iconSize,
-            width: iconSize,
-          ),
-        ),
-        const SizedBox(height: 5),
-        const Text("Carbs: 20g", style: TEXT_PLAIN) //TODO: replace with post values
-      ]),
-      const SizedBox(width: 30),
-      Column(children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(iconSize / 2),
-          child: Image.asset(
-            'assets/images/Proteins.png',
-            height: iconSize,
-            width: iconSize,
-          ),
-        ),
-        const SizedBox(height: 5),
-        const Text("Proteins: 13g", style: TEXT_PLAIN) //TODO: replace with post values
-      ])
-    ]);
-  }
-
   Widget _buildCreatorRow(BuildContext context) {
     return Row(
       children: [
@@ -121,42 +71,16 @@ class PostScreen extends StatelessWidget {
           style: HOME_POST_CREATOR,
         ),
         const SizedBox(width: 160),
-        const Icon(Icons.calendar_month_rounded, size: 13),
+        const Padding(
+            padding: EdgeInsets.only(bottom: 3),
+            child: Icon(Icons.calendar_month_rounded, size: 13)),
         const SizedBox(width: 5),
         Text(
-            "${post.creationDate.day}.${post.creationDate.month}.${post.creationDate.year}",
+            "${post.creationDate.day}.${post.creationDate.month}.${post
+                .creationDate.year}",
             style: TEXT_BOLD)
       ],
     );
-  }
-
-  Widget _buildImage(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.only(
-          top: 20,
-        ),
-        child: AspectRatio(
-          aspectRatio: 2,
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: Image.asset('assets/images/${post.recipe.title}.png',
-                //TODO: mit gespeichertem bild aus datenbank ersetzen
-                fit: BoxFit.fill),
-          ),
-        ));
-  }
-
-  Widget _buildCommunity(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.only(top: 20),
-        child: Column(
-          children: [
-            _buildTextRow('Comments:', post.comments?.first ?? "no comments"),
-            _buildTextRow('Likes:', post.likes?.first.name ?? "no likes"),
-            _buildTextRow(
-                'Reactions:', post.reactions?.toString() ?? "no reactions"),
-          ],
-        ));
   }
 
   Widget _buildProfileIcon(BuildContext context) {
@@ -177,63 +101,74 @@ class PostScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildTextRow(String key, String value) {
-    return Row(
-      children: [
-        Text(
-          key,
-          style: TEXT_BOLD,
-        ),
-        const SizedBox(width: 8),
-        Expanded(
-          child: Text(
-            value,
-            style: TEXT_PLAIN,
-            overflow: TextOverflow.ellipsis,
-            textAlign: TextAlign.end,
-          ),
-        ),
-      ],
-    );
+  Widget _buildDescription(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.only(top: 20),
+        child: Text(post.recipe.description, style: TEXT_PLAIN));
   }
 
-  Widget _buildContainerCaption(
-      BuildContext context, Widget widget, String caption) {
+  Widget _buildNutrition() {
+    const double iconSize = 54;
+    return Row(children: [
+      const SizedBox(width: 43),
+      Column(children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(iconSize / 2),
+          child: Image.asset(
+            'assets/images/Fats.png',
+            height: iconSize,
+            width: iconSize,
+          ),
+        ),
+        const SizedBox(height: 5),
+        const Text("Fats: 5g", style: TEXT_PLAIN)
+        //TODO: replace with post values
+      ]),
+      const SizedBox(width: 30),
+      Column(children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(iconSize / 2),
+          child: Image.asset(
+            'assets/images/Carbs.png',
+            height: iconSize,
+            width: iconSize,
+          ),
+        ),
+        const SizedBox(height: 5),
+        const Text("Carbs: 20g", style: TEXT_PLAIN)
+        //TODO: replace with post values
+      ]),
+      const SizedBox(width: 28),
+      Column(children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(iconSize / 2),
+          child: Image.asset(
+            'assets/images/Proteins.png',
+            height: iconSize,
+            width: iconSize,
+          ),
+        ),
+        const SizedBox(height: 5),
+        const Text("Proteins: 13g", style: TEXT_PLAIN)
+        //TODO: replace with post values
+      ])
+    ]);
+  }
+
+  Widget _buildImage(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 20),
-      child: Stack(
-        children: <Widget>[
-          Container(
-              margin: const EdgeInsets.only(top: 10),
-              padding: const EdgeInsets.only(top: 15, bottom: 10),
-              //width: 400,
-              decoration: BoxDecoration(
-                  color: COLOR_WHITE,
-                  border: Border.all(width: 3, color: COLOR_INDIGO_LIGHT),
-                  borderRadius: BorderRadius.circular(12)),
-              child: Padding(
-                  padding: const EdgeInsets.only(
-                      left: 5, top: 5, right: 5, bottom: 5),
-                  child: Column(
-                    children: [
-                      widget,
-                    ],
-                  ))),
-          Positioned(
-              left: 30,
-              top: 0,
-              child: Container(
-                padding:
-                    const EdgeInsets.only(bottom: 2, top: 2, left: 7, right: 7),
-                color: COLOR_WHITE,
-                child: Text(
-                  caption,
-                  style: POST_CAPTION_INDIGO_LIGHT,
-                ),
-              )),
-        ],
-      ),
-    );
+        padding: const EdgeInsets.only(
+          top: 20,
+        ),
+        child: AspectRatio(
+          aspectRatio: 2,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.asset('assets/images/${post.recipe.title}.png',
+                //TODO: mit gespeichertem bild aus datenbank ersetzen
+                fit: BoxFit.fill),
+          ),
+        ));
   }
 
   Widget _buildIngredients(BuildContext context) {
@@ -282,5 +217,88 @@ class PostScreen extends StatelessWidget {
             ]);
           }).toList(),
         ));
+  }
+
+  Widget _buildCommunity(BuildContext context) {
+    //TODO: add reactions
+    //TODO: add comments
+    return Padding(
+        padding: const EdgeInsets.only(top: 20),
+        child: Column(
+          children: [
+            Padding(
+                padding: const EdgeInsets.only(right: 310),
+                child: IconButton(
+                  icon: Icon(
+                    _favIconOutlined,
+                    color: COLOR_RED,
+                    size: 27,
+                  ),
+                  onPressed: () {
+                    //TODO: add state to widget?
+                  },
+                )),
+            Padding(padding: const EdgeInsets.only(top: 20),
+                child: _buildTextRow("Comments:", "No comments yet"))
+          ],
+        ));
+  }
+
+  Widget _buildContainerCaption(BuildContext context, Widget widget,
+      String caption) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child: Stack(
+        children: <Widget>[
+          Container(
+              margin: const EdgeInsets.only(top: 10),
+              padding: const EdgeInsets.only(top: 15, bottom: 10),
+              decoration: BoxDecoration(
+                  color: COLOR_WHITE,
+                  border: Border.all(width: 3, color: COLOR_INDIGO_LIGHT),
+                  borderRadius: BorderRadius.circular(12)),
+              child: Padding(
+                  padding: const EdgeInsets.only(
+                      left: 5, top: 5, right: 5, bottom: 5),
+                  child: Column(
+                    children: [
+                      widget,
+                    ],
+                  ))),
+          Positioned(
+              left: 30,
+              top: 0,
+              child: Container(
+                padding:
+                const EdgeInsets.only(bottom: 2, top: 2, left: 7, right: 7),
+                color: COLOR_WHITE,
+                child: Text(
+                  caption,
+                  style: POST_CAPTION_INDIGO_LIGHT,
+                ),
+              )),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTextRow(String key, String value) {
+    return Row(
+      children: [
+        Text(
+          key,
+          style: TEXT_BOLD,
+        ),
+        const SizedBox(width: 8),
+        Expanded(
+          child: Text(
+            value,
+            style: TEXT_PLAIN,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.end,
+          ),
+        ),
+      ],
+    );
   }
 }
